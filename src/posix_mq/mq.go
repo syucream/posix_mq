c71159cc21fe1e47c774fabe092d8b46bbb61a6a
Ryo Okubo <syucream1031@gmail.com>
2018-10-12 01:06:46 +0900

    Separate C code and Go code

--- src/posix_mq/mq.go
+++ src/posix_mq/mq.go
@@ -1,15 +1,5 @@
 package posix_mq
 
-/*
-#include <mqueue.h>
-
-// Expose non-variadic function requires 2 arguments.
-mqd_t mq_open2(const char *name, int oflag) {
-	return mq_open(name, oflag);
-}
-*/
-import "C"
-
 // Represents the message queue
 type MessageQueue struct {
 	handler int
@@ -17,12 +7,12 @@ type MessageQueue struct {
 
 // NewMessageQueue returns an instance of the message queue given a QueueConfig.
 func NewMessageQueue(name string, oflag int) (*MessageQueue, error) {
-	h, err := C.mq_open2(C.CString(name), C.int(oflag))
+	h, err := mq_open(name, oflag)
 	if err != nil {
 		return nil, err
 	}
 
 	return &MessageQueue{
-		handler: int(h),
+		handler: h,
 	}, nil
 }
---
+++ src/posix_mq/wrapper.go
@@ -0,0 +1,20 @@
+package posix_mq
+
+/*
+#include <mqueue.h>
+
+// Expose non-variadic function requires 2 arguments.
+mqd_t mq_open2(const char *name, int oflag) {
+	return mq_open(name, oflag);
+}
+*/
+import "C"
+
+func mq_open(name string, oflag int) (int, error) {
+	h, err := C.mq_open2(C.CString(name), C.int(oflag))
+	if err != nil {
+		return 0, err
+	}
+
+	return int(h), nil
+}
